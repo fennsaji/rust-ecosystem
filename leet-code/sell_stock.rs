0a30788805c21dfc4f2a1e48b21e3269c252d9bb
Fenn Saji <fenn@swnglobal.com>
2025-08-05 19:42:20 +0530

    Added sell stock

---
+++ leet-code/sell_stock.rs
@@ -0,0 +1,54 @@
+pub fn max_profit(prices: Vec<i32>) -> i32 {
+    let mut min_price = i32::MAX;
+    let mut max_profit = 0;
+
+    for price in prices {
+        if price < min_price {
+            min_price = price;
+        } else {
+            let profit = price - min_price;
+            if profit > max_profit {
+                max_profit = profit;
+            }
+        }
+    }
+
+    max_profit
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;   
+    #[test]
+    fn test_max_profit_basic() {
+        assert_eq!(max_profit(vec![7, 1, 5, 3, 6, 4]), 5);
+    }   
+    #[test]
+    fn test_max_profit_no_profit() {
+        assert_eq!(max_profit(vec![7, 6, 4, 3, 1]), 0);
+    }
+    #[test]
+    fn test_max_profit_single_day() {
+        assert_eq!(max_profit(vec![1]), 0);
+    }
+    #[test]
+    fn test_max_profit_multiple_days() {
+        assert_eq!(max_profit(vec![1, 2, 3, 4, 5]), 4);
+    }
+    #[test]
+    fn test_max_profit_same_price() {
+        assert_eq!(max_profit(vec![5, 5, 5, 5]), 0);
+    }
+    #[test]
+    fn test_max_profit_increasing_prices() {
+        assert_eq!(max_profit(vec![1, 2, 3, 4, 5, 6]), 5);
+    }
+    #[test]
+    fn test_max_profit_decreasing_prices() {
+        assert_eq!(max_profit(vec![6, 5, 4, 3, 2, 1]), 0);
+    }
+    #[test]
+    fn test_max_profit_large_numbers() {
+        assert_eq!(max_profit(vec![1000, 2000, 3000, 4000, 5000]), 4000);
+    }
+}
